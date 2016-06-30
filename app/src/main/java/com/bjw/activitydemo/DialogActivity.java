74dc618fbb653e69f84cd63c265d9d114be8bf9b
lelouch0823 <lelouch0823@hotmail.com>
2016-06-30 09:55:42 +0800

    Logger

--- app/src/main/java/com/bjw/activitydemo/DialogActivity.java
+++ app/src/main/java/com/bjw/activitydemo/DialogActivity.java
@@ -4,14 +4,52 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by Administrator on 2016/6/29 0029.
  */
 public class DialogActivity extends Activity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Logger.d("hello");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Logger.d("hello");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Logger.d("hello");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Logger.d("hello");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Logger.d("hello");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Logger.d("hello");
+    }
+
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_activity);
-
+        Logger.d("hello");
     }
 }
--- app/src/main/java/com/bjw/activitydemo/NormalActivity.java
+++ app/src/main/java/com/bjw/activitydemo/NormalActivity.java
@@ -2,10 +2,11 @@ package com.bjw.activitydemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+
+import com.orhanobut.logger.Logger;
 
 /**
  * Created by Administrator on 2016/6/29 0029.
@@ -13,46 +14,48 @@ import android.widget.Button;
 public class NormalActivity extends Activity {
     public static final String TAG = "NormalActivity Out Message";
 
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy: ");
+        Logger.d("hello");
+        //Log.d(TAG, "onDestroy: ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume: ");
+        Logger.d("hello");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop: ");
+        Logger.d("hello");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause: ");
+        Logger.d("hello");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onRestart: ");
+        Logger.d("hello");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart: ");
+        Logger.d("hello");
     }
 
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.normal_activity);
-
+        Logger.d("hello");
         Button button = (Button) findViewById(R.id.destroy_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
